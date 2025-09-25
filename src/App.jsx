92c37b420acb6c7478163ff92d43b48683317602
RAKIB <rasaleh19@gmail.com>
2025-09-25 12:27:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button class="btn btn-success">Success</button>
+      <button className="btn btn-error">Error</button>
+    </>
+  );
+}
+
+export default App;
